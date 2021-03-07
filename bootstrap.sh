6ef568cb9bd0bd934a9ab0cf8fe4397bad4596b5
Zhenyu Yang <nocis@tju.edu.cn>
2021-03-07 06:51:07 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ cd $HOME/.local
 # 0. save root password
 echo "enter the sudo password, please"
 read PW
-echo -e "\e[1;32m [root password saved successful] \e[0m"
+echo "\e[1;32m [root password saved successful] \e[0m"
 
 #echo $PW | ./playback_delete_data_patch.sh 09_delete_old_data_p.sql  
 #./command_wo_sudo.sh <param>
@@ -12,31 +12,31 @@ echo -e "\e[1;32m [root password saved successful] \e[0m"
 
 # 1. update
 echo $PW | sudo -S apt-get update > /dev/null 2>&1
-echo -e "\e[1;32m [pkgs update successful] \e[0m"
+echo "\e[1;32m [pkgs update successful] \e[0m"
 
 # 2. check if git is installed
 echo $PW | sudo -S apt-get -fy install git > /dev/null 2>&1
-echo -e "\e[1;32m [git install successful] \e[0m"
+echo "\e[1;32m [git install successful] \e[0m"
 
 # 4. fetch scripts from 
 git clone https://github.com/nocis/shell-scripts.git > /dev/null 2>&1
 cd shell-scripts
 cd ..
 git pull --recurse-submodules
-echo -e "\e[1;32m [clone scripts successfull] \e[0m"
+echo "\e[1;32m [clone scripts successfull] \e[0m"
 
 # 5. auto install packages
 echo $PW | yes | ~/.local/shell-scripts/utils/install.sh
-echo -e "\e[1;32m [pkgs install successful] \e[0m"
+echo "\e[1;32m [pkgs install successful] \e[0m"
 
 # append init.sh scrirt to bashrc
 if grep -Fxq "init.sh" ~/.bashrc
 then
     # if found
-    echo -e "\e[1;31m init.sh is already loaded \e[0m"
+    echo "\e[1;31m init.sh is already loaded \e[0m"
 else
     # if not found
     echo 'source ~/.local/shell-scripts/init/init.sh' >> ~/.bashrc 
     source ~/.local/shell-scripts/init/init.sh 
 fi
-echo -e "\e[1;32m [init.sh activate successful] \e[0m"
+echo "\e[1;32m [init.sh activate successful] \e[0m"
