2fe22c0bd968b7720565793cb850364bf2f38db2
Zhenyu Yang <nocis@tju.edu.cn>
2021-03-07 08:32:18 -0800

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -2,5 +2,9 @@
 # 1. install c++ build essential
 echo $PW | sudo -kS apt-get -fy install build-essential > /dev/null 2>&1
 
+#check gcc
+#sudo update-alternatives --config gcc
+g++ --version
+
 # 2. install OGL
 echo $PW | sudo -kS apt-get -fy install cmake libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev > /dev/null 2>&1
