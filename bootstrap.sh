2470e48edab325f04f9bb6467570e3281e92ed22
Zhenyu Yang <nocis@tju.edu.cn>
2021-03-07 07:52:36 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,11 +11,11 @@ echo "\e[1;32m [root password saved successful] \e[0m"
 #echo $PW | ./other_command_requires_sudo.sh <param>
 
 # 1. update
-echo $PW | sudo -S apt-get update > /dev/null 2>&1
+echo $PW | sudo -kS apt-get update > /dev/null 2>&1
 echo "\e[1;32m [pkgs update successful] \e[0m"
 
 # 2. check if git is installed
-echo $PW | sudo -S apt-get -fy install git #> /dev/null 2>&1
+echo $PW | sudo -kS apt-get -fy install git #> /dev/null 2>&1
 echo "\e[1;32m [git install successful] \e[0m"
 
 # 4. fetch scripts from 
