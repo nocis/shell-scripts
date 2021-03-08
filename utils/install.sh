ffe13fec145a200fedaa26d3c1182a04209180e4
Zhenyu Yang <nocis@tju.edu.cn>
2021-03-08 01:50:24 -0800

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -7,23 +7,31 @@ echo -e "\e[1;33m --[c++ build essential installed] \e[0m"
 echo $PW | sudo -kS apt-get -fy install libatlas3-base libwebp6 libtiff5 libjasper1 libilmbase23 libopenexr23 libavcodec58 libavformat58 libavutil56 libswscale5 libgtk-3-0 libpangocairo-1.0-0 libpango-1.0-0 libatk1.0-0 libcairo-gobject2 libcairo2 libgdk-pixbuf2.0-0 libqtgui4 libqt4-test libqtcore4 > /dev/null 2>&1
 echo -e "\e[1;33m --[CV pre-requisites installed] \e[0m"
 
+# 3. install ffmpeg
+echo $PW | sudo -kS apt-get -fy install ffmpeg > /dev/null 2>&1
+echo -e "\e[1;33m --[FFMPEG installed] \e[0m"
+
+# 4. install openexr
+echo $PW | sudo -kS apt-get -fy install openexr > /dev/null 2>&1
+echo -e "\e[1;33m --[OpenExr installed] \e[0m"
+
 #check gcc
 #sudo update-alternatives --config gcc
 #g++ --version
 
-# 3. install OGL and eigen3
+# 5. install OGL and eigen3
 echo $PW | sudo -kS apt-get -fy install cmake libx11-dev xorg-dev libglu1-mesa-dev freeglut3-dev libglew1.5 libglew1.5-dev libglu1-mesa libglu1-mesa-dev libgl1-mesa-glx libgl1-mesa-dev libeigen3-dev > /dev/null 2>&1
 echo -e "\e[1;33m --[OGL libs installed] \e[0m"
 
-# 4. install open-vm-tools
+# 6. install open-vm-tools
 echo $PW | sudo -kS apt-get -fy install open-vm-tools open-vm-tools-desktop > /dev/null 2>&1
 echo -e "\e[1;33m --[open-vm-tools installed] \e[0m"
 
-# 5. install Qt pre-requisites
+# 7. install Qt pre-requisites
 echo $PW | sudo -kS apt-get -fy install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 > /dev/null 2>&1
 echo -e "\e[1;33m --[Qt pre-requisites installed] \e[0m"
 
-# 6. install CLion
+# 8. install CLion
 read -p "Do you want to install CLion（snap required）? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -34,7 +42,7 @@ else
     echo -e "\e[1;31m --[CLion not installed] \e[0m"
 fi
 
-# 7. install vscode
+# 9. install vscode
 read -p "Do you want to install vscode（snap required）? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -45,7 +53,7 @@ else
     echo -e "\e[1;31m --[vscode not installed] \e[0m"
 fi
 
-# 8. install cmake
+# 10. install cmake
 read -p "Do you want to install cmake（snap required）? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
@@ -56,7 +64,7 @@ else
     echo -e "\e[1;31m --[cmake not installed] \e[0m"
 fi
 
-# 9. install gitkraken
+# 11. install gitkraken
 read -p "Do you want to install gitkraken（snap required）? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
