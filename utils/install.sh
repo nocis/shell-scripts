279d054e70db505f058469e189b60323fd3f6cea
Zhenyu Yang <nocis@tju.edu.cn>
2021-03-08 02:00:35 -0800

    Update install.sh

--- utils/install.sh
+++ utils/install.sh
@@ -10,7 +10,7 @@ echo -e "\e[1;33m --[CV pre-requisites installed] \e[0m"
 # 3. install ffmpeg
 echo $PW | sudo -kS apt-get -fy install libavutil-dev libavcodec-dev libavformat-dev libopenexr-dev libav-tools libavcodec-extra-56 ffmpeg > /dev/null 2>&1
 echo $PW | sudo -kS apt-get upgrade
-echo $PW | sudo -kS apt --fix-broken install
+echo $PW | sudo -kS apt -y --fix-broken install
 echo -e "\e[1;33m --[FFMPEG installed] \e[0m"
 
 # 4. install openexr
