ad45f998a87cc72dc986e09f83ca3f1a44b0c7fc
zy <nocis@tju.edu.cn>
2021-03-16 11:10:14 -0700

    node init

--- utils/functions.sh
+++ utils/functions.sh
@@ -3,7 +3,7 @@ check_installed()
 {
     hash $1 > /dev/null 2>&1
     if [ "$?" != "0" ]; then
-        echo -e "\e[1;31m [$1 is not found] \e[0m"
+        echo -e "\e[1;31m [$1 not found] \e[0m"
         exit 1
     else
         echo -e "\e[1;32m [$1 is installed] \e[0m"
